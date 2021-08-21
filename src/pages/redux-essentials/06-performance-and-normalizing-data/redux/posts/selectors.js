5a0f08ee883b50d0ad87ea2b04aadf0d55375ef4
jatwing <jatwing@pm.me>
2021-08-22 00:25:47 +0800

    2. finished the majority of 06, React.memo and createEntityAdapter remains.

--- src/pages/redux-essentials/06-performance-and-normalizing-data/redux/posts/selectors.js
+++ src/pages/redux-essentials/06-performance-and-normalizing-data/redux/posts/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit'
+
 /** extracting posts selectos */
 export const selectAllPosts = (state) => state.posts.data;
 
@@ -10,3 +12,9 @@ export const selectPostsIds = (state) =>
 export const selectPostsStatus = (state) => state.posts.status;
 
 export const selectPostsError = (state) => state.posts.error;
+
+/** memoizing selector functions */
+export const selectPostsByUser = createSelector(
+  [selectAllPosts, (state, userId) => userId],
+  (posts, userId) => posts.filter(post => post.user === userId)
+)
--- src/pages/redux-essentials/06-performance-and-normalizing-data/view/user-page.js
+++ src/pages/redux-essentials/06-performance-and-normalizing-data/view/user-page.js
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 import { selectUserById, selectAllUsers } from '../redux/users/selectors';
-import { selectAllPosts } from '../redux/posts/selectors';
+import { selectAllPosts, selectPostsByUser } from '../redux/posts/selectors';
 import { useState } from 'react';
 import { UserSelect } from './user-select';
 
@@ -9,12 +9,17 @@ export const UserPage = () => {
   const [userId, setUserId] = useState('');
   const users = useSelector(selectAllUsers);
 
-  const user = useSelector((state) => selectUserById(state, userId));
+  const user = useSelector((state) => selectUserById(state, userId))
 
+  const postsForUser = useSelector(state => selectPostsByUser(state, userId))
+
+/*
   const postsForUser = useSelector((state) => {
     const allPosts = selectAllPosts(state);
     return allPosts.filter((post) => post.user === userId);
   });
+*/
+
 
   const postTitles = postsForUser.map((post) => (
     <li key={post.id}>{post.title}</li>
