1d459bccae7ff0a9fe9d5ba5669830dba1616a1b
jatwing <jatwing@pm.me>
2021-09-14 00:43:23 +0800

    1. use the getEntitiesUrls() function in firestore.js (actually only work with saga) to simplify the asset url fetched.

--- src/redux/projects/sagas.js
+++ src/redux/projects/sagas.js
@@ -1,9 +1,7 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
 import { createAction } from '@reduxjs/toolkit';
 import { collection, getDocs } from 'firebase/firestore/lite';
-import { db, storage } from 'src/utils/firebase';
-import { ref, getDownloadURL } from 'firebase/storage';
-import { zipObject, zipWith } from 'lodash';
+import { db, getEntitiesUrls} from 'src/utils/firebase';
 
 export const projectsFetched = createAction('projects/projectsFetched');
 export const projectsFetchedPending = createAction(
@@ -26,81 +24,11 @@ const fetchProjects = async () => {
   }));
 };
 
-function* getEntityUrl(entity, key) {
-  const imageRef = ref(storage, entity[key]);
-  const url = yield call(getDownloadURL, imageRef);
-  return {
-    ...entity,
-    [key]: url
-  }
-}
-
-function* getEntityUrls(entity, keys) {
-  const imageRefs = keys.map((key) => ref(storage, entity[key]));
-  const descriptions = imageRefs.map((imageRef) =>
-    call(getDownloadURL, imageRef)
-  );
-  const urls = yield all(descriptions);
-  return {
-    ...entity,
-    ...zipObject(keys, urls)
-  }
-}
-
-function* getEntitiesUrls(entities, keys) {
-  const descriptions = entities.map(entity =>
-    call(getEntityUrls, entity, keys)
-  )
-  const result = yield all(descriptions)
-  return result;
-}
-
-
-
-
-
 function* workProjectsFetched() {
   try {
     yield put(projectsFetchedPending());
-    const projects = yield call(fetchProjects);
-
-    console.log('test getEntityUrl here');
-    const result1 = yield* getEntityUrl(projects[0], 'avatar');
-    console.log(result1);
-
-    console.log('test getEntityUrls  here');
-    const result2 = yield* getEntityUrls(projects[0], ['avatar', 'test']);
-    console.log(result2);
-
-    console.log('test getEntitiesUrls  here');
-    const result3 = yield* getEntitiesUrls(projects, ['avatar', 'test']);
-    console.log(result3)
-
-
-/** Version 2
-    const descriptions = projects.map((project) => {
-      const imageRef = ref(storage, project.avatar);
-      return call(getDownloadURL, imageRef);
-    });
-    const urls = yield all(descriptions);
-
-    zipWith(projects, urls, (project, url) => {
-      project.avatar = url;
-    });
-*/
-
-
-    /*
-      Version 1
-    for (let i = 0; i < projects.length; i++) {
-      const imageRef = ref(storage, projects[i].avatar);
-      const url = yield call(getDownloadURL, imageRef)
-      projects[i].avatar = url;
-
-      console.log(projects[i])
-    }
-*/
-
+    let projects = yield call(fetchProjects);
+    projects = yield* getEntitiesUrls(projects, ['avatar']);
     yield put(projectsFetchedFulfilled(projects));
   } catch (error) {
     yield put(projectsFetchedRejected(error));
@@ -111,21 +39,3 @@ export function* watchProjectsFetched() {
   yield takeLatest(projectsFetched, workProjectsFetched);
 }
 
-/*
-
- similar to authors but require an image.
-
-  const path ='images/jatwing-avatar.png'
-  const imageRef = ref(storage, path);
-  getDownloadURL(imageRef).then(
-    url => {
-      console.log(url)
-    }
-  ).catch(error => {
-    console.log(error)
-  })
-
-
-  useEffect(() => {
- 
- */
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,6 +1,9 @@
 import { initializeApp } from 'firebase/app';
-import {  getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { getStorage } from 'firebase/storage';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
+import { zipObject } from 'lodash';
+import { call, all } from 'redux-saga/effects';
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -10,11 +13,34 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 
-export const storage = getStorage(app)
+export function* getEntityUrl(entity, key) {
+  const imageRef = ref(storage, entity[key]);
+  const url = yield call(getDownloadURL, imageRef);
+  return {
+    ...entity,
+    [key]: url,
+  };
+}
 
+export function* getEntityUrls(entity, keys) {
+  const imageRefs = keys.map((key) => ref(storage, entity[key]));
+  const descriptions = imageRefs.map((imageRef) =>
+    call(getDownloadURL, imageRef)
+  );
+  const urls = yield all(descriptions);
+  return {
+    ...entity,
+    ...zipObject(keys, urls),
+  };
+}
 
-
+export function* getEntitiesUrls(entities, keys) {
+  const descriptions = entities.map((entity) =>
+    call(getEntityUrls, entity, keys)
+  );
+  return yield all(descriptions);
+}
