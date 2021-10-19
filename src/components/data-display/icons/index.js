efe53139be7794090f3f94769531b366057bd534
jatwing <jatwing@pm.me>
2021-10-20 00:21:03 +0800

    1. simplify the icon code, gonna to use gray intensity.

--- src/components/data-display/icons/index.js
+++ src/components/data-display/icons/index.js
@@ -30,32 +30,36 @@ export const JatwingIcon = (props) => {
 };
 
 export const ReactNotesIcon = (props) => {
-    return <SvgIcon {...props}>
-
-  <line x1="14.535" y1="16.396" x2="19.604" y2="7.604" 
-  style={{
-    strokeWidth: '8.792px',
-    strokeLinecap: 'round',
-    stroke: '#86d46b',
-    fill: 'none',
-  }}
-  />
-  <line x1="9.465" y1="7.604" x2="14.535" y2="16.396"
-    style={{
-      strokeWidth: '8.792px',
-      strokeLinecap: 'round',
-      stroke: '#764abc',
-      fill: 'none',
-    }}
-   />
-  <line x1="4.396" y1="16.396" x2="9.465" y2="7.604" 
-    style={{
-      strokeWidth: '8.792px',
-      strokeLinecap: 'round',
-      stroke: '#61dafb',
-      fill: 'none',
-    }}
-  />
-
+  return (
+    <SvgIcon {...props}>
+      <path
+        d="M 14.535 16.396 L 19.604 7.604"
+        style={{
+          strokeWidth: '8.792px',
+          strokeLinecap: 'round',
+          stroke: '#86d46b',
+          /** rgb(134, 212, 107) */
+          fill: 'none',
+        }}
+      />
+      <path
+        d="M 9.465 7.604 L 14.535 16.396"
+        style={{
+          strokeWidth: '8.792px',
+          strokeLinecap: 'round',
+          stroke: '#764abc', /** rgb(118, 74, 188) */
+          fill: 'none',
+        }}
+      />
+      <path
+        d="M 4.396 16.396 L 9.465 7.604"
+        style={{
+          strokeWidth: '8.792px',
+          strokeLinecap: 'round',
+          stroke: '#61dafb', /** rgb(97, 218, 251) */
+          fill: 'none',
+        }}
+      />
     </SvgIcon>
-}
+  );
+};
