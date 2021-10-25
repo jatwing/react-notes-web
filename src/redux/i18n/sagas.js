77e57608c2d6917ba2001771175be920cc56e2be
jatwing <jatwing@pm.me>
2021-10-25 23:37:41 +0800

    4. TODO rewrite i18n/sagas.

--- src/redux/i18n/sagas.js
+++ src/redux/i18n/sagas.js
@@ -1,10 +1,26 @@
 import { instanceInitialized, resourcesAdded, languageChanged } from './slice';
 import { call, put, take, all } from 'redux-saga/effects';
 
+/**
+ * old functions are just acceptable, conside new requirements.
+ * 1. divide t and l, so the entities sagas can choose t, l or both.
+ * 2. split their logic instead of combining them,
+ *    the helper functions are NOT very helpful.
+ *
+ * some ideas:
+ * 0. assume we do know the configs of i18n.
+ * 1. l takes instanceInitialized first then take every languageChanged,
+ *    it may put 'localizationAccessible'.
+ * 2. t takes resourcesAdded first then take every languageChanged,
+ *    it may put 'translationAccessible'.
+ * 3. task of this file should be sending reliable messages
+ *    about t and l, to the entities outside.
+ */
+
 /** watchers */
 export function* watchEntitesLocalization(localizationAction) {
-  const  {
-    payload: { t, l }
+  const {
+    payload: { t, l },
   } = yield take([resourcesAdded, languageChanged]);
 
   while (true) {
@@ -22,13 +38,13 @@ export function* watchEntitiesOperationWithLocalization(
 ) {
   const [
     _,
-    __,
+    // __,
     {
       payload: { t, l },
     },
   ] = yield all([
     take(operationAction),
-    take(instanceInitialized),
+    // take(instanceInitialized),
     take([resourcesAdded, languageChanged]),
   ]);
   yield call(operationWorker);
