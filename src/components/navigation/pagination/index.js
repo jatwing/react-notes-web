b15b81da311c0310de2c5722c5462615428647d3
jatwing <jatwing@pm.me>
2021-10-28 10:53:38 +0800

    1. simplify pagination component and modify accessibility.

--- src/components/navigation/pagination/index.js
+++ src/components/navigation/pagination/index.js
@@ -1,113 +1,78 @@
 import { Box, Card, Typography } from '@mui/material';
 import { LinkBase } from 'src/components/navigation/link';
 
+const PaginationCard = (props) => {
+  const { page, label } = props;
+  if (!page) {
+    return (
+      <Box
+        sx={{
+          flex: '1 0',
+          maxWidth: '50%',
+        }}
+      />
+    );
+  }
+  return (
+    <LinkBase
+      href={page.url ?? '/'}
+      sx={{
+        flex: '1 0',
+        maxWidth: '50%',
+      }}
+    >
+      <Card
+        variant="outlined"
+        sx={{
+          height: '100%',
+          color: page.color ?? 'primary.main',
+          p: '16px',
+          '&:hover': {
+            borderColor: 'currentcolor',
+            '& > .MuiTypography-subtitle1': {
+              color: 'currentcolor',
+            },
+          },
+        }}
+      >
+        <Typography
+          children={label}
+          variant="subtitle2"
+          component="h6"
+          sx={{ color: 'text.secondary', mb: '4px' }}
+        />
+        <Typography
+          children={page.name ? `« ${page.name}` : '«'}
+          variant="subtitle1"
+          component="h1"
+          sx={{
+            fontWeight: 'bold',
+            color: 'text.primary',
+          }}
+        />
+      </Card>
+    </LinkBase>
+  );
+};
+
 export const SimplePagination = (props) => {
   const { previousLabel, nextLabel, previousPage, nextPage } = props;
-
   if (!previousPage && !nextPage) {
     return <></>;
   }
-
   return (
-    <Box sx={{ display: 'flex' }}>
-      {!previousPage ? (
-        <Box
-          sx={{
-            flex: '1 0',
-            maxWidth: '50%',
-            mr: '16px',
-          }}
-        />
-      ) : (
-        <LinkBase
-          href={previousPage.url ?? '/'}
-          sx={{
-            flex: '1 0',
-            maxWidth: '50%',
-            mr: '16px',
-          }}
-        >
-          <Card
-            variant="outlined"
-            sx={{
-              height: '100%',
-              textAlign: 'left',
-              color: previousPage.color ?? 'text.primary',
-              p: '16px',
-              '&:hover': {
-                borderColor: 'currentcolor',
-                '& > .MuiTypography-subtitle1': {
-                  color: 'currentcolor',
-                },
-              },
-            }}
-          >
-            <Typography
-              children={previousLabel}
-              variant="subtitle2"
-              sx={{ color: 'text.secondary', mb: '4px' }}
-            />
-            <Typography
-              children={`« ${previousPage.name ?? ''}`}
-              variant="subtitle1"
-              sx={{
-                fontWeight: 'bold',
-                color: 'text.primary',
-              }}
-            />
-          </Card>
-        </LinkBase>
-      )}
-
-      {!nextPage ? (
-        <Box
-          sx={{
-            flex: '1 0',
-            maxWidth: '50%',
-          }}
-        />
-      ) : (
-        <LinkBase
-          href={nextPage.url ?? ''}
-          sx={{
-            flex: '1 0',
-            maxWidth: '50%',
-          }}
-        >
-          <Card
-            variant="outlined"
-            sx={{
-              height: '100%',
-              textAlign: 'right',
-              color: nextPage.color ?? 'text.primary',
-              p: '16px',
-              '&:hover': {
-                borderColor: 'currentcolor',
-                '& > .MuiTypography-subtitle1': {
-                  color: 'currentcolor',
-                },
-              },
-            }}
-          >
-            <Typography
-              children={nextLabel}
-              variant="subtitle2"
-              sx={{
-                color: 'text.secondary',
-                mb: '4px',
-              }}
-            />
-            <Typography
-              children={`${nextPage.name ?? ''} »`}
-              variant="subtitle1"
-              sx={{
-                color: 'text.primary',
-                fontWeight: 'bold',
-              }}
-            />
-          </Card>
-        </LinkBase>
-      )}
+    <Box
+      component="nav"
+      sx={{
+        display: 'flex',
+        '& > .MuiLink-root:nth-child(2)': {
+          textAlign: 'right',
+          ml: '16px'
+        },
+      }}
+    >
+      <PaginationCard page={previousPage} label={previousLabel} />
+      <PaginationCard page={nextPage} label={nextLabel} />
     </Box>
   );
 };
